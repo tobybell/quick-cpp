35dd139893cb6c5a8c6331925cea219f317cb9f8
Toby Bell <tbell@stanford.edu>
2023-08-15 23:55:45 -0700

    Parse some function prototypes

--- demo/lib.cc
+++ demo/lib.cc
@@ -3,3 +3,14 @@
 int my_func() {
   return 5;
 }
+
+void my_other_function() {
+  return;
+  if (4) {
+    return;
+  }
+}
+
+float my_third_function() {
+  return 3.14;
+}
--- quick.cc
+++ quick.cc
@@ -7,6 +7,17 @@
 #include <unistd.h>
 #include <utility>
 
+// Next work to do:
+// Create some sort of on-disk database of all function definitions, indexed by
+// name.
+// because of overloading, single name can map to multiple functions
+// produce function definition strings, which are just the range from the
+// start of the return type to the closing brace
+// the full function definition string is what gets compiled. if it ever
+// changes, or if its dependent prototypes change, it gets recompiled.
+// Otherwise, it doesn't need to get recompiled
+
+
 namespace {
 
 using std::exchange;
@@ -58,6 +69,12 @@ struct String {
   }
   char& operator[](unsigned index) { return data[index]; }
   char const& operator[](unsigned index) const { return data[index]; }
+  operator Str() const { return {data, size}; }
+};
+
+struct CStr {
+  String storage;
+  operator char const*() const { return &storage[0]; }
 };
 
 struct BytesOut {
@@ -79,14 +96,15 @@ struct BytesOut {
     return &storage[size];
   }
 
-  String take() {
-    storage.resize(size);
+  CStr take_null_terminated() {
+    storage.resize(size + 1);
+    storage[size] = '\0';
     size = 0;
-    return move(storage);
+    return {move(storage)};
   }
 };
 
-String read_file(char const* path) {
+CStr read_file(char const* path) {
   int fd = open(path, 0, O_RDONLY);
   check(fd >= 0);
 
@@ -102,13 +120,113 @@ String read_file(char const* path) {
   }
 
   close(fd);
-  return out.take();
+  return out.take_null_terminated();
 }
 
-struct CStr {
-  String storage;
-  operator char const*() const { return &storage[0]; }
+struct Token {
+  unsigned start;
+  unsigned size;
 };
+
+unsigned memsize(char const&) { return 1; }
+unsigned memsize(Str const& s) { return s.size; }
+template <unsigned N>
+unsigned memsize(char const (&)[N]) {
+  return N;
+}
+
+void put(char*& cursor, char const& c) { *cursor++ = c; }
+void put(char*& cursor, Str const& s) { memcpy(cursor, s.data, s.size); cursor += s.size; }
+template <unsigned N>
+void put(char*& cursor, char const (&s)[N]) { memcpy(cursor, s, N); cursor += N; }
+
+template <class... T>
+void sprint(BytesOut& out, T const&... args) {
+  auto needed = (... + memsize(args));
+  auto cursor = out.reserve(needed);
+  (put(cursor, args),...);
+  out.size = static_cast<unsigned>(cursor - out.storage.data);
+}
+
+using u8 = unsigned char;
+
+bool is_alphanum(char const& c) {
+  return u8(c - 'A') < 26 || u8(c - 'a') < 26 || u8(c - '0') < 10;
+}
+
+bool is_identifier(char const& c) {
+  return u8(c - 'A') < 26 || u8(c - 'a') < 26 || u8(c - '0') < 10 || c == '_';
+}
+
+void whitespace(char const*& it) {
+  while (*it == ' ' || *it == '\n')
+    ++it;
+}
+
+void parse_one_function(char const*& it, BytesOut& prototype) {
+  auto return_type = it;
+  while (is_alphanum(*it))
+    ++it;
+  sprint(prototype, Str {return_type, static_cast<unsigned>(it - return_type)}, ' ');
+
+  whitespace(it);
+
+  auto name = it;
+  while (is_identifier(*it))
+    ++it;
+  sprint(prototype, Str {name, static_cast<unsigned>(it - name)}, '(');
+
+  whitespace(it);
+  check(*it == '(');
+  ++it;
+
+  whitespace(it);
+
+  while (*it != ')') {
+    auto type = it;
+    while (is_identifier(*it))
+      ++it;
+    sprint(prototype, Str {type, static_cast<unsigned>(it - type)});
+    whitespace(it);
+
+    if (*it == ',') {
+      ++it;
+      whitespace(it);
+      sprint(prototype, ", ");
+    }
+  }
+  ++it;
+  whitespace(it);
+
+  sprint(prototype, ");\n");
+
+  check(*it == '{');
+  unsigned level = 1;
+  while (true) {
+    ++it;
+    if (*it == '{') {
+      ++level;
+    } else if (*it == '}') {
+      --level;
+      if (!level)
+        break;
+    }
+  }
+  ++it;  // '}'
+  whitespace(it);
+}
+
+void parse_prototypes(char const* source) {
+  char const* it = source;
+
+  BytesOut prototype;
+
+  whitespace(it);
+  while (*it)
+    parse_one_function(it, prototype);
+
+  write(STDERR_FILENO, prototype.storage.data, prototype.size);
+}
 
 CStr path_join(char const* first, char const* second) {
   unsigned n_first = strlen(first);
@@ -129,8 +247,9 @@ void read_directory(char const* path) {
   struct dirent* dir;
   while ((dir = readdir(d))) {
     if (ends_with(Str::from(dir->d_name), ".cc"_s)) {
-      String contents = read_file(path_join(path, dir->d_name));
-      write(STDERR_FILENO, contents.data, contents.size);
+      CStr contents = read_file(path_join(path, dir->d_name));
+      write(STDERR_FILENO, contents.storage.data, contents.storage.size - 1);
+      parse_prototypes(contents);
     }
   }
   closedir(d);
